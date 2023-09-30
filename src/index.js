e2a3eef6c35812b542a0d170bfdbadb4df7d7442
michael <michaelngige@gmail.com>
2023-09-30 12:21:48 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+   // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+    const inputField = document.getElementById("new-task-description");
+    const taskDescription = inputField.value;
+
+    if (taskDescription.trim() !== "") { 
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskDescription;
+
+      taskList.appendChild(taskItem);      
+      inputField.value = "";
+    }
+  });
 });
